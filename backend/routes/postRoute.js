b496760e7b73682fc7c86f2643011435b83df1d0
MEHEDI HASAN MARUF <137112466+maruf6890@users.noreply.github.com>
2026-06-08 21:40:43 +0600

    update: ui updates

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -3,12 +3,14 @@ import { isAuthenticated } from "../middleware/isAuthenticated.js"
 import {
 deletePost,
 getPostById,
+getPosts
 } from "../controllers/postController.js";
 
 
 const router = express.Router()
 router.delete("/:postId", isAuthenticated, deletePost);
 router.get("/:postId", isAuthenticated, getPostById);
+router.get("/", isAuthenticated, getPosts);
 export default router
 
 
