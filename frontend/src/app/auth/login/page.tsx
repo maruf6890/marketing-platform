33d9d895e3370307286db7336f7a7cef0fb6a4e8
Md. Nazmul Alam <nazmulalamtuhin11011@gmail.com>
2026-06-08 23:19:24 +0600

    feat: Implement authentication layout and remove unused dashboard page

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,5 +1,4 @@
 import LoginForm from '@/components/auth/LoginForm'
-import React from 'react'
 
 export default function page() {
     return <LoginForm/>
--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,6 +1,4 @@
-import LoginForm from '@/components/auth/LoginForm'
 import SignUpForm from '@/components/auth/SignUpForm'
-import React from 'react'
 
 export default function page() {
     return <SignUpForm/>
