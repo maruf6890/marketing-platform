ccc7d2c6291889802cd149ebe94df17e87f7ce6b
Md. Nazmul Alam <nazmulalamtuhin11011@gmail.com>
2026-06-09 22:22:08 +0600

    feat: add logger on file

--- backend/routes/facebookRoute.js
+++ backend/routes/facebookRoute.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { isAuthenticated } from "../middleware/isAuthenticated.js"
+import { Logger } from "../middleware/logger.js";
 import {
   reqUsersPageList,
   getUsersPageList,
@@ -14,15 +15,15 @@ import {
 
 
 const router = express.Router()
-router.get("/request_pages", isAuthenticated, reqUsersPageList);
-router.get("/page_list", isAuthenticated, getUsersPageList);
-router.post("/create_post", isAuthenticated, createPost);
-router.put("/edit_post/:postId", isAuthenticated, editPost);
-router.post("/publish_post", isAuthenticated, publishScheduledOrDraftPost);
-router.get("/drafts", isAuthenticated, getDraftsPosts);
-router.get("/scheduled_posts", isAuthenticated, getScheduledPosts);
-router.get("/feed", isAuthenticated, getFacebookFeed);
-router.post("/comments/:postId", isAuthenticated, getPostComments);
+router.get("/request_pages", isAuthenticated, Logger, reqUsersPageList);
+router.get("/page_list", isAuthenticated, Logger, getUsersPageList);
+router.post("/create_post", isAuthenticated, Logger, createPost);
+router.put("/edit_post/:postId", isAuthenticated, Logger, editPost);
+router.post("/publish_post", isAuthenticated, Logger, publishScheduledOrDraftPost);
+router.get("/drafts", isAuthenticated, Logger, getDraftsPosts);
+router.get("/scheduled_posts", isAuthenticated, Logger, getScheduledPosts);
+router.get("/feed", isAuthenticated, Logger, getFacebookFeed);
+router.post("/comments/:postId", isAuthenticated, Logger, getPostComments);
 export default router
 
 
